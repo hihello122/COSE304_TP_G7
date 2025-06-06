973495fae56d8cb3bfd14271e73bdcf7f5247029
hihello122 <tprmatpxkr@gmail.com>
2025-06-07 08:42:22 +0900

    structure changed

---
+++ train.py
@@ -0,0 +1,337 @@
+import argparse
+import time
+import math
+import os
+import os.path
+import numpy as np
+from tqdm import tqdm
+import gc
+
+import torch
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+import torchvision.datasets as vdsets
+
+
+from resflows.resflow import ACT_FNS, ResidualFlow
+import resflows.datasets as datasets
+import resflows.optimizers as optim
+import resflows.utils as utils
+import resflows.layers as layers
+import resflows.layers.base as base_layers
+from resflows.lr_scheduler import CosineAnnealingWarmRestarts
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--data', type=str, default='cifar10', choices=[
+        'mnist',
+        'cifar10'
+    ]
+)
+args = parser.parse_args()
+
+if args.seed is None:
+    args.seed = np.random.randint(100000)
+
+utils.makedirs(args.save)
+logger = utils.get_logger(logpath=os.path.join(args.save, 'logs'), filepath=os.path.abspath(__file__))
+logger.info(args)
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+if device.type == 'cuda':
+    torch.cuda.manual_seed(args.seed)
+
+
+def geometric_logprob(ns, p):
+    return torch.log(1 - p + 1e-10) * (ns - 1) + torch.log(p + 1e-10)
+
+def standard_normal_sample(size):
+    return torch.randn(size)
+
+def standard_normal_logprob(z):
+    logZ = -0.5 * math.log(2 * math.pi)
+    return logZ - z.pow(2) / 2
+
+def normal_logprob(z, mean, log_std):
+    mean = mean + torch.tensor(0.)
+    log_std = log_std + torch.tensor(0.)
+    c = torch.tensor([math.log(2 * math.pi)]).to(z)
+    inv_sigma = torch.exp(-log_std)
+    tmp = (z - mean) * inv_sigma
+    return -0.5 * (tmp * tmp + 2 * log_std + c)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def reduce_bits(x):
+    if args.nbits < 8:
+        x = x * 255
+        x = torch.floor(x / 2**(8 - args.nbits))
+        x = x / 2**args.nbits
+    return x
+
+
+def add_noise(x, nvals=256):
+    """
+    [0, 1] -> [0, nvals] -> add noise -> [0, 1]
+    """
+    if args.add_noise:
+        noise = x.new().resize_as_(x).uniform_()
+        x = x * (nvals - 1) + noise
+        x = x / nvals
+    return x
+
+
+def update_lr(optimizer, itr):
+    iter_frac = min(float(itr + 1) / max(args.warmup_iters, 1), 1.0)
+    lr = args.lr * iter_frac
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
+
+def add_padding(x, nvals=256):
+    if args.padding > 0:
+        if args.padding_dist == 'uniform':
+            u = x.new_empty(x.shape[0], args.padding, x.shape[2], x.shape[3]).uniform_()
+            logpu = torch.zeros_like(u).sum([1, 2, 3]).view(-1, 1)
+            return torch.cat([x, u / nvals], dim=1), logpu
+        elif args.padding_dist == 'gaussian':
+            u = x.new_empty(x.shape[0], args.padding, x.shape[2], x.shape[3]).normal_(nvals / 2, nvals / 8)
+            logpu = normal_logprob(u, nvals / 2, math.log(nvals / 8)).sum([1, 2, 3]).view(-1, 1)
+            return torch.cat([x, u / nvals], dim=1), logpu
+        else:
+            raise ValueError()
+    else:
+        return x, torch.zeros(x.shape[0], 1).to(x)
+
+
+def remove_padding(x):
+    if args.padding > 0:
+        return x[:, :im_dim, :, :]
+    else:
+        return x
+    
+if args.data == 'cifar10':
+    im_dim = 3
+    n_classes = 10
+    transform_train = transforms.Compose([
+        transforms.Resize(args.imagesize),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        add_noise,
+    ])
+    transform_test = transforms.Compose([
+        transforms.Resize(args.imagesize),
+        transforms.ToTensor(),
+        add_noise,
+    ])
+    init_layer = layers.LogitTransform(0.05) if args.logit_transform else None
+    train_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10(args.dataroot, train=True, transform=transform_train),
+        batch_size=args.batchsize,
+        shuffle=True,
+        num_workers=args.nworkers,
+    )
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10(args.dataroot, train=False, transform=transform_test),
+        batch_size=args.val_batchsize,
+        shuffle=False,
+        num_workers=args.nworkers,
+    )
+elif args.data == 'mnist':
+    im_dim = 1
+    init_layer = layers.LogitTransform(1e-6) if args.logit_transform else None
+    n_classes = 10
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(
+            args.dataroot, train=True, transform=transforms.Compose([
+                transforms.Resize(args.imagesize),
+                transforms.ToTensor(),
+                add_noise,
+            ])
+        ),
+        batch_size=args.batchsize,
+        shuffle=True,
+        num_workers=args.nworkers,
+    )
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(
+            args.dataroot, train=False, transform=transforms.Compose([
+                transforms.Resize(args.imagesize),
+                transforms.ToTensor(),
+                add_noise,
+            ])
+        ),
+        batch_size=args.val_batchsize,
+        shuffle=False,
+        num_workers=args.nworkers,
+    )
+
+
+
+# Optimization
+def tensor_in(t, a):
+    for a_ in a:
+        if t is a_:
+            return True
+    return False
+
+
+scheduler = None
+optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.99), weight_decay=args.wd)
+logger.info(optimizer)
+
+fixed_z = standard_normal_sample([min(32, args.batchsize),
+                                  (im_dim + args.padding) * args.imagesize * args.imagesize]).to(device)
+
+criterion = torch.nn.CrossEntropyLoss()
+
+
+
+def get_lipschitz_constants(model):
+    lipschitz_constants = []
+    for m in model.modules():
+        if isinstance(m, base_layers.SpectralNormConv2d) or isinstance(m, base_layers.SpectralNormLinear):
+            lipschitz_constants.append(m.scale)
+        if isinstance(m, base_layers.InducedNormConv2d) or isinstance(m, base_layers.InducedNormLinear):
+            lipschitz_constants.append(m.scale)
+        if isinstance(m, base_layers.LopConv2d) or isinstance(m, base_layers.LopLinear):
+            lipschitz_constants.append(m.scale)
+    return lipschitz_constants
+
+
+def update_lipschitz(model):
+    with torch.no_grad():
+        for m in model.modules():
+            if isinstance(m, base_layers.SpectralNormConv2d) or isinstance(m, base_layers.SpectralNormLinear):
+                m.compute_weight(update=True)
+            if isinstance(m, base_layers.InducedNormConv2d) or isinstance(m, base_layers.InducedNormLinear):
+                m.compute_weight(update=True)
+
+def get_ords(model):
+    ords = []
+    for m in model.modules():
+        if isinstance(m, base_layers.InducedNormConv2d) or isinstance(m, base_layers.InducedNormLinear):
+            domain, codomain = m.compute_domain_codomain()
+            if torch.is_tensor(domain):
+                domain = domain.item()
+            if torch.is_tensor(codomain):
+                codomain = codomain.item()
+            ords.append(domain)
+            ords.append(codomain)
+    return ords
+
+batch_time = utils.RunningAverageMeter(0.97)
+bpd_meter = utils.RunningAverageMeter(0.97)
+logpz_meter = utils.RunningAverageMeter(0.97)
+deltalogp_meter = utils.RunningAverageMeter(0.97)
+firmom_meter = utils.RunningAverageMeter(0.97)
+secmom_meter = utils.RunningAverageMeter(0.97)
+gnorm_meter = utils.RunningAverageMeter(0.97)
+ce_meter = utils.RunningAverageMeter(0.97)
+
+def train(epoch,model):
+
+    model = torch.nn.DataParallel(model)
+
+    end = time.time()
+
+    for i,(x,y) in enumerate (train_loader):
+        x = x.to(device)
+        global_itr = epoch * len(train_loader) + i
+        beta = beta = min(1, global_itr / args.annealing_iters) if args.annealing_iters > 0 else 1.
+
+        #bpd,logits, logpz, neg_delta_logp = compute_loss(x,model,beta=beta)
+
+        bits_per_dim, logits_tensor = torch.zeros(1).to(x), torch.zeros(n_classes).to(x)
+        logpz, delta_logp = torch.zeros(1).to(x), torch.zeros(1).to(x)
+        nvals = 256
+        x, logpu = add_padding(x, nvals)
+
+        z, delta_logp = model(x.view(-1, *input_size[1:]), 0)
+        logpz = standard_normal_logprob(z).view(z.size(0), -1).sum(1, keepdim=True)
+
+        # log p(x)
+        logpx = logpz - beta * delta_logp - np.log(nvals) * (
+            args.imagesize * args.imagesize * (im_dim + args.padding)
+        ) - logpu
+        bits_per_dim = -torch.mean(logpx) / (args.imagesize * args.imagesize * im_dim) / np.log(2)
+        logpz = torch.mean(logpz).detach()
+        neg_delta_logp = torch.mean(-delta_logp).detach()
+
+
+        #optimizer
+        if global_itr % args.update_freq == args.update_freq - 1:
+
+            if args.update_freq > 1:
+                with torch.no_grad():
+                    for p in model.parameters():
+                        if p.grad is not None:
+                            p.grad /= args.update_freq
+
+            grad_norm = torch.nn.utils.clip_grad.clip_grad_norm_(model.parameters(), 1.)
+            if args.learn_p: compute_p_grads(model)
+
+            optimizer.step()
+            optimizer.zero_grad()
+            update_lipschitz(model)
+            ema.apply()
+
+            gnorm_meter.update(grad_norm)
+
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+
+        del x
+
+
+    s = (
+            'Epoch: [{0}][{1}/{2}] | Time {batch_time.val:.3f} | '
+            'GradNorm {gnorm_meter.avg:.2f}'.format(
+                epoch, i, len(train_loader), batch_time=batch_time, gnorm_meter=gnorm_meter
+            )
+    )
+    logger.info(s)
+    visualize(epoch,model,i,x)
+    torch.cuda.empty_cache()
+    gc.collect()
+
+
+def main():
+    global best_test_bpd
+
+    last_checkpoints = []
+    lipschitz_constants = []
+    ords = []
+
+    for epoch in range(args.begin_epoch, args.nepochs):
+        train(epoch,model)
+        lipschitz_constants.append(get_lipschitz_constants(model))
+        ords.append(get_ords(model))
+
+        test_bpd = validate(epoch,model)
+        scheduler.step()
+
+        if test_bpd < best_test_bpd:
+            best_test_bpd = test_bpd
+            utils.save_checkpoint({
+                'state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'args': args,
+                'ema': ema,
+                'test_bpd': test_bpd,
+            }, os.path.join(args.save, 'models'), epoch, last_checkpoints, num_checkpoints=5)
+
+        torch.save({
+            'state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'args': args,
+            'ema': ema,
+            'test_bpd': test_bpd,
+        }, os.path.join(args.save, 'models', 'most_recent.pth'))
